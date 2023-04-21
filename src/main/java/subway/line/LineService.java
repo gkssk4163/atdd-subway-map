eb68efe67e70fbd1acdfaa39fb081b70a5c6418d
Hanna Choi <gkssk4163@hanmail.net>
2023-04-21 17:37:56 +0900

    feat: 노선등록 및 노선목록조회 기능 구현

--- src/main/java/subway/line/LineService.java
+++ src/main/java/subway/line/LineService.java
@@ -2,9 +2,7 @@ package subway.line;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import subway.station.Station;
-import subway.station.StationRequest;
-import subway.station.StationResponse;
+import subway.station.StationRepository;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -14,7 +12,39 @@ import java.util.stream.Collectors;
 public class LineService {
     private LineRepository lineRepository;
 
-    public LineService(LineRepository lineRepository) {
+    private StationRepository stationRepository;
+
+
+    public LineService(LineRepository lineRepository, StationRepository stationRepository) {
         this.lineRepository = lineRepository;
+        this.stationRepository = stationRepository;
+    }
+
+    @Transactional
+    public LineResponse saveLine(LineRequest lineRequest) {
+        Line line = lineRepository.save(
+                new Line(lineRequest.getName(),
+                        lineRequest.getColor(),
+                        stationRepository.findById(lineRequest.getUpStationId()).get(),
+                        stationRepository.findById(lineRequest.getDownStationId()).get(),
+                        lineRequest.getDistance()
+                ));
+        return createLineResponse(line);
+    }
+
+    public List<LineResponse> findAllLines() {
+        return lineRepository.findAll().stream()
+                .map(this::createLineResponse)
+                .collect(Collectors.toList());
+    }
+
+    private LineResponse createLineResponse(Line line) {
+        return new LineResponse(
+                line.getId(),
+                line.getName(),
+                line.getColor(),
+                line.getUpStation(),
+                line.getDownStation()
+        );
     }
 }
